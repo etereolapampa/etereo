0a0f9ed7c4a63b77d6d750385a348af3f5f3210d
rusoens <ezeschiller@gmail.com>
2025-06-12 19:30:25 -0300

    revisando detalles finales

--- client/src/api.js
+++ client/src/api.js
@@ -2,8 +2,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000',
+  baseURL: import.meta.env.VITE_API_URL,
 });
+
 
 // Interceptor para agregar JWT en cada request
 api.interceptors.request.use(config => {
